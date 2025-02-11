29cc71c04f84d15774548a93b8cd3f6fcf491684
duyld <duyld@milemarker.io>
2025-02-11 13:44:07 +0700

    add sentry

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -5,20 +5,37 @@ import { QueryClient, QueryClientProvider } from "react-query";
 const queryClient = new QueryClient();
 import Reactotron from "reactotron-react-native";
 import AsyncStorage from "@react-native-async-storage/async-storage";
+import * as Sentry from "@sentry/react-native";
 import "../global.css";
+import { useEffect } from "react";
+import ErrorBoundary from "./errors/ErrorBoundary";
+
+if (process.env.SENTRY_DSN) {
+  Sentry.init({
+    dsn: process.env.SENTRY_DSN,
+    tracesSampleRate: 1.0,
+    enableAppHangTracking: false,
+  });
+}
 
 if (process.env.NODE_ENV === "development") {
   Reactotron.setAsyncStorageHandler(AsyncStorage)
-    .configure({name: "React Native"})
+    .configure({ name: "React Native" })
     .useReactNative()
     .connect();
 }
 
 export default function RootLayout() {
+  useEffect(() => {
+    Sentry.captureMessage("App Started! 🚀");
+  }, []);
+
   return (
     <Provider store={store}>
       <QueryClientProvider client={queryClient}>
-        <Stack screenOptions={{ headerShown: false }} />
+        <ErrorBoundary>
+          <Stack screenOptions={{ headerShown: false }} />
+        </ErrorBoundary>
       </QueryClientProvider>
     </Provider>
   );
